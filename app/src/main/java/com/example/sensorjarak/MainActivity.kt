8917a6e83cedeca58103dfc56e7a7d4aff5d00b4
ridhodzaki <ridhodzaki03@gmail.com>
2022-10-01 12:32:57 +0700

    first commit

---
+++ app/src/main/java/com/example/sensorjarak/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.sensorjarak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("test/range")
+
+
+
+    }
+}
